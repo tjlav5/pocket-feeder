c4c8c61a3913403cf90330ff168db7b4f16a5dc3
TJ Lavelle <tjlav5@gmail.com>
2015-08-21 19:48:53 -0400

    add state mock

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -4,6 +4,7 @@ describe('Controller: MainCtrl', function () {
 
   // load the controller's module
   beforeEach(module('pocketFeederApp'));
+  beforeEach(module('stateMock'));
   beforeEach(module('socketMock'));
 
   var MainCtrl,
